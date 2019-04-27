061a41803ba2f4bd42eea67e02f5cc4bd53dcd06
ospupegam <ospupegam@yahoo.com>
2019-04-27 22:04:31 +0200

    v2 ass2 ngs2

--- ass2_1_ngs2.sh
+++ ass2_1_ngs2.sh
@@ -35,4 +35,28 @@ samtools faidx /media/ngs-01/My_Passport/ngs2workspace/ngs2_ass/chr22/chr22_with
 java -jar gatk SplitNCigarReads -R /media/ngs-01/My_Passport/ngs2workspace/ngs2_ass/chr22/chr22_with_ERCC92.fa -I dedupped.bam -O split.bam
 
 #indexing
+picard_path=$CONDA_PREFIX/share/picard-2.19.2-0
+java -Xmx2g -jar $picard_path/picard.jar BuildBamIndex VALIDATION_STRINGENCY=LENIENT INPUT=split.bam
+
+java -Xmx2g -jar $picard_path/picard.jar CreateSequenceDictionary R=/media/ngs-01/My_Passport/ngs2workspace/ngs2_ass/chr22/chr22_with_ERCC92.fa O=./chr22_with_ERCC92.dict
+
+samtools faidx /media/ngs-01/My_Passport/ngs2workspace/ngs2_ass/chr22/chr22_with_ERCC92.fa
+
+# Download known varinats
+
+wget ftp://ftp.ensembl.org/pub/grch37/current/variation/vcf/homo_sapiens/homo_sapiens-chr22.vcf.gz -O chr22.vcf.gz
+gunzip chr22.vcf.gz
+
+#grep "^#" chr22.vcf > fam_chr22.vcf
+#grep "^22" chr22.vcf | sed 's/^22/22 dna_sm:chromosome chromosome:GRCh38:22:1:50818468:1 REF
+/' >> fam_chr22.vcf
+#gatk IndexFeatureFile -F fam_chr22.vcf
+
+# base recalibration:
+#gatk --java-options "-Xmx2G" BaseRecalibrator -R /media/ngs-01/My_Passport/ngs2workspace/ngs2_ass/chr22/chr22_with_ERCC92.fa -I split.bam --known-sites fam_chr22.vcf -O split.report
+
+#BQSR
+#gatk --java-options "-Xmx2G" ApplyBQSR -R /media/ngs-01/My_Passport/ngs2workspace/ngs2_ass/chr22/chr22_with_ERCC92.fa -I split.bam -bqsr split.report -O split.bqsr.bam --add-output-sam-program-record --emit-original-quals
+
+
 
